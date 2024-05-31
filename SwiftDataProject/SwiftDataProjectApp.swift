9b2a0168b3f270c1c8d69e44d96b4b5a6880ff63
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-31 13:16:36 -0700

    Create model context and container

--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Dechon Ryan on 5/31/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
